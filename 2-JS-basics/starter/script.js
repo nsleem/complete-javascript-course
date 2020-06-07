9424c63a20a5ec3a750f138ef8596c20f40676c5
nsleem <nsleem94@gmail.com>
2020-06-06 21:17:04 -0700

    Learn Variable Mutation and Type Coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,21 +1,43 @@
 /*************************
 * Variables and Data Types
 **************************/
-var firstName = 'Nader';
-console.log(firstName);
 
-var lastName = "Sleem";
-var age = 25;
-var fullAge = true;
-console.log(fullAge);
+// var firstName = 'Nader';
+// console.log(firstName);
 
-var job
-// Logs 'undefined' to the console
-console.log(job)
+// var lastName = "Sleem";
+// var age = 25;
+// var fullAge = true;
+// console.log(fullAge);
 
-job = 'Teacher'
-console.log(job)
+// var job
+// // Logs 'undefined' to the console
+// console.log(job)
+
+// job = 'Teacher'
+// console.log(job)
 
 /**************************************
  * Variable mutation and type coercion
  *************************************/
+
+var firstName = 'Nader';
+var age = 25;
+// Type Coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable Mutation
+
+age = 'twenty five'
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
