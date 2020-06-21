a704a67e4e4596bb3e604c90b21ff1dafbfbf60a
nsleem <nsleem94@gmail.com>
2020-06-20 22:32:01 -0700

    Implement Hold Button and Game End State

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,12 +9,12 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer;
 
 
 
 scores = [0,0];
-roundScores = 0;
+roundScore = 0;
 activePlayer = 0;
 
 
@@ -43,24 +43,46 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // 3. Update the roundScore IF the rolled number was NOT a 1.
     if (dice !== 1) {
         // Add score
-        roundScores += dice;
-        document.querySelector("#current-" + activePlayer).textContent = roundScores;
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
     } else {
         // Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScores = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-
-        diceDOM.style.display = 'none';
-
-
+        nextPlayer();
     }
-
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    // Update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Next Player
+        nextPlayer();
+    }
+});
+
+
+function nextPlayer() {
+    // Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
