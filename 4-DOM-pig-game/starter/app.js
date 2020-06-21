f526ad8ffd297973c4d2c02a3dc89ffdbeb87c2e
nsleem <nsleem94@gmail.com>
2020-06-20 22:43:09 -0700

    Add Game Init function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,24 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-
-
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-
-//dice = Math.floor(Math.random() * 6) + 1;
-//document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-1').textContent = 0;
-
-document.querySelector('.dice').style.display = 'none';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
@@ -70,6 +53,33 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    // Need to remove first and then add again. Otherwise we might accidentally,
+    // have two active classes attached to the element.
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
 
 function nextPlayer() {
     // Next Player
@@ -86,3 +96,26 @@ function nextPlayer() {
 
     document.querySelector('.dice').style.display = 'none';
 }
+
+
+
+
+
+
+
+
+// scores = [0,0];
+// roundScore = 0;
+// activePlayer = 0;
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
+
+// document.getElementById('score-0').textContent = 0;
+// document.getElementById('current-0').textContent = 0;
+// document.getElementById('score-1').textContent = 0;
+// document.getElementById('current-1').textContent = 0;
+
+// document.querySelector('.dice').style.display = 'none';
