059ade8590eecb0038c08756cb663a55f2ae7631
nsleem <nsleem94@gmail.com>
2020-07-04 14:56:34 -0700

    Display Month/Year above Budget

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -157,7 +157,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -273,6 +274,18 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function() {
+            var now, month, months, year;
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -374,6 +387,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return {
         init: function() {
+            UIController.displayMonth();
             UIController.displayBudget({
                 budget: 0,
                 totalInc: 0,
