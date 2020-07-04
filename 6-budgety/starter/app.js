4b094aefd40f328daf798197f48d03395556a880
nsleem <nsleem94@gmail.com>
2020-07-03 20:50:27 -0700

    Delete Item from Budget Data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -66,6 +66,20 @@ var budgetController = (function() {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculdateBudget: function() {
 
             // Calculate total income and expenses
@@ -242,10 +256,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         if (itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]); // Need to convert the string to integer
 
             // Delete the item from the data structure
-
+            budgetController.deleteItem(type, ID);
             // Delete the item from the UI
 
             // Update and Show the new budget
